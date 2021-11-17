e849d7cd28b3fcfc00e5a5abd05766c7d2d21b5a
HackerPoet <901189+HackerPoet@users.noreply.github.com>
2021-11-16 21:42:05 -0800

    Use the exact CPU flag for raspberry pi zero.

--- os/raspberry-pi1.cmake
+++ os/raspberry-pi1.cmake
@@ -10,7 +10,7 @@ endif()
 include(polly_add_cache_flag)
 
 # -mcpu=arm1176jzf-s not compatible, removed
-foreach(_flag -mfpu=vfp)
+foreach(_flag -mfpu=vfp -mcpu=arm1176jzf-s)
   polly_add_cache_flag(CMAKE_C_FLAGS   "${_flag}")
   polly_add_cache_flag(CMAKE_CXX_FLAGS "${_flag}")
 endforeach()
